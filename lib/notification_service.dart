d5c06baada9be379590d4698621f66c5645866ed
David Pinheiro <davidaspinheiro@gmail.com>
2026-01-18 22:30:14 +0100

    Remove unecessary prints

--- lib/notification_service.dart
+++ lib/notification_service.dart
@@ -3,41 +3,37 @@ import 'database_service.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
 class NotificationService {
-  final FirebaseMessaging _firebaseMessaging = FirebaseMessaging.instance;
-  final DatabaseService _dbService = DatabaseService();
+  final FirebaseMessaging messaging = FirebaseMessaging.instance;
+  final DatabaseService dataBase = DatabaseService();
 
-  // Inicializar Notificações
+  //Inicialize notifications
   Future<void> initNotifications() async {
-    // 1. Pedir permissão
-    NotificationSettings settings = await _firebaseMessaging.requestPermission(
+    //Request permission for notifications
+    NotificationSettings settings = await messaging.requestPermission(
       alert: true,
       badge: true,
       sound: true,
     );
 
     if (settings.authorizationStatus == AuthorizationStatus.authorized) {
-      print('User granted permission');
       
-      // 2. Obter o Token
-      String? token = await _firebaseMessaging.getToken();
-      if (token != null) {
-        print("FCM Token: $token");
-        // Guardar na base de dados se o user estiver logado
+      //Get the token for this device (used to send notifications to it)
+      String? userToken = await messaging.getToken();
+      //Save the token to your database when the user is logged in
+      if (userToken != null) {
         final user = FirebaseAuth.instance.currentUser;
         if (user != null) {
-          await _dbService.saveUserToken(user.uid, token);
+          await dataBase.saveUserToken(user.uid, userToken);
         }
       }
 
-      // 3. Ouvir mensagens em primeiro plano (Foreground)
+      //Get notified when a message is received while the app is in the foreground
       FirebaseMessaging.onMessage.listen((RemoteMessage message) {
         print('Got a message whilst in the foreground!');
         print('Message data: ${message.data}');
 
         if (message.notification != null) {
           print('Message also contained a notification: ${message.notification}');
-          // Aqui podes usar o flutter_local_notifications para mostrar um popup
-          // Se quiseres, posso dar-te esse código extra depois.
         }
       });
     }
